02cd2604460705df66f71be902bc6d4ade622b20
Andrés <andres.maneiro@automattic.com>
2018-12-05 17:46:05 +0100

    Call puppeteer APIs

--- tests/e2e/index.test.js
+++ tests/e2e/index.test.js
@@ -1,5 +1,9 @@
 describe( 'Block settings menu', () => {
-	it( 'contains a new item added by this plugin', () => {
+	it( 'contains a new item added by this plugin', async () => {
+		const page = await browser.newPage();
+		await page.goto( 'http://localhost:8889/wp-admin/post-new.php', {
+			timeout: 5000,
+		} );
 		expect( false ).toBe( true );
-	});
+	} );
 } );
