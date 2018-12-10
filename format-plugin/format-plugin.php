09ffee64d2075c7d89aed7d20232a591ee5961f1
Andrés <andres.maneiro@automattic.com>
2018-12-10 20:35:35 +0100

    Add CSS to format plugin

--- format-plugin/format-plugin.php
+++ format-plugin/format-plugin.php
@@ -9,14 +9,19 @@
 
 function format_plugin_script_register() {
 	wp_register_script(
-		'format-plugin',
+		'format-plugin-js',
 		plugins_url( 'format-plugin.js', __FILE__ ),
 		array( 'wp-editor', 'wp-element', 'wp-rich-text' )
+	);
+	wp_register_style(
+		'format-plugin-css',
+		plugins_url( 'format-plugin.css', __FILE__ )
 	);
 }
 add_action( 'init', 'format_plugin_script_register' );
 
 function format_plugin_script_enqueue() {
-	wp_enqueue_script( 'format-plugin' );
+	wp_enqueue_script( 'format-plugin-js' );
+	wp_enqueue_style( 'format-plugin-css' );
 }
 add_action( 'enqueue_block_editor_assets', 'format_plugin_script_enqueue' );
