844339685e2700aca09bc816a71e7a0f0b1cc919
Andrés <andres.maneiro@automattic.com>
2018-12-12 19:01:26 +0100

    Initial version of reactive-docs plugin

---
+++ reactive-docs/editor.js
@@ -0,0 +1,4 @@
+/**
+ * Internal dependencies
+ */
+import 'gutenberg/extensions/reactive-docs/editor';
---
+++ reactive-docs/engine.js
@@ -0,0 +1,8 @@
+/** @format */
+
+( function( wp ) {
+	function doTheMagic() {
+		console.log( 'do the magic', document.getElementsByClassName( 'reactivedocs-input' ) );
+	}
+	wp.domReady( doTheMagic );
+} )( window.wp );
