4becddf72fbdc3b374af9bfe357eb19c83b4cad7
Andrés <andres.maneiro@automattic.com>
2019-02-08 12:16:02 +0100

    Add minimal meta box

---
+++ meta-box/meta-box.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Plugin name: Demo Meta
+ */
+
+function demo_meta_box_add() {
+	add_meta_box(
+		'demo-meta-box',
+		'Demo Meta Box',
+		'__return_empty_string',
+		'post',
+		'normal',
+		'high',
+		array(
+			'__back_compat_meta_box' => false,
+			'__block_editor_compatible_meta_box' => false,
+		)
+	);
+}
+add_action( 'add_meta_boxes', 'demo_meta_box_add' );
