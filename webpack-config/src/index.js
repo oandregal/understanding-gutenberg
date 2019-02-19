d27d8f4a50d991f01a450521a70ddc03796fe16d
Andrés <andres.maneiro@automattic.com>
2019-02-19 18:15:31 +0100

    Use ESNext example

--- webpack-config/src/index.js
+++ webpack-config/src/index.js
@@ -1,27 +1,22 @@
-( function( wp ) {
-    var el = wp.element.createElement;
-    wp.blocks.registerBlockType( 'blockexample-plugin/block', {
-        title: 'Love',
-        icon: 'heart',
-        category: 'common',
-        attributes: {
-            activity: {
-                type: 'string',
-            },
-        },
-        edit: function( props ) {
-            return ( el(
-                'p',
-                {},
-                'I ♥ Gutenberg.'
-            ) );
-        },
-        save: function( props ) {
-            return ( el(
-                'p',
-                {},
-                'I ♥ .'
-            ) );
-        },
-    } );
-} )( window.wp );
+import { createElement } from '@wordpress/element';
+import { registerBlockType } from '@wordpress/blocks';
+
+registerBlockType( 'blockexample-plugin/block', {
+    title: 'Love',
+    icon: 'heart',
+    category: 'common',
+    edit: function() {
+        return ( createElement(
+            'p',
+            {},
+            'I ♥ Gutenberg.'
+        ) );
+    },
+    save: function() {
+        return ( createElement(
+            'p',
+            {},
+            'I ♥ .'
+        ) );
+    },
+} );
