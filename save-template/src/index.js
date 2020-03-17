ef5d474bad1da11bb758221098ab9327d55bb406
Andrés <andres.maneiro@automattic.com>
2020-03-17 18:08:17 +0100

    Add createTemplateFromBlocks function

--- save-template/src/index.js
+++ save-template/src/index.js
@@ -1,24 +1,48 @@
 /**
+ * External dependencies
+ */
+import { map } from 'lodash';
+
+/**
  * WordPress dependencies
  */
 import { createElement } from '@wordpress/element';
 import { PluginBlockSettingsMenuItem } from '@wordpress/edit-post';
 import { registerPlugin } from '@wordpress/plugins';
 import { useSelect } from '@wordpress/data';
+import { serialize } from '@wordpress/blocks';
+
+const createTemplateFromBlocks = ( blocks = [] ) => {
+	if ( ! Array.isArray( blocks ) || blocks.length === 0 ) {
+		return [];
+	}
+
+	return map( blocks, ( { name, attributes, innerBlocks } ) => {
+		return [ name, attributes, createTemplateFromBlocks( innerBlocks ) ];
+	} );
+};
 
 function SaveTemplate() {
 	const selectedBlocks = useSelect( ( select ) => {
 		const selectors = select( 'core/block-editor' );
 		return selectors.hasMultiSelection()
 			? selectors.getMultiSelectedBlocks()
-			: selectors.getSelectedBlock();
+			: [ selectors.getSelectedBlock() ];
 	} );
 	return createElement(
 		PluginBlockSettingsMenuItem,
 		{
 			label: 'Save template',
 			onClick: function() {
-				console.log( 'Blocks selected are ', selectedBlocks );
+				console.log( 'Blocks ', selectedBlocks );
+				console.log(
+					'Template ',
+					createTemplateFromBlocks( selectedBlocks )
+				);
+				console.log(
+					'Serialized blocks ',
+					serialize( selectedBlocks )
+				);
 			},
 		},
 		null
